c1970eb61ac2a6dc4c8d7ec80e8b1784c1fec96c
Yevgeni Anischenko <yevkemail@gmail.com>
2021-02-01 17:01:24 +0200

    Add project files.

---
+++ ConctactsApp/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace ConctactsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtboxName = new System.Windows.Forms.TextBox();
+            this.txtboxSurname = new System.Windows.Forms.TextBox();
+            this.txtboxPhone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.myConctactsDBDataSet = new ConctactsApp.MyConctactsDBDataSet();
+            this.myConctactsDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myConctactsDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myConctactsDBDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtboxName
+            // 
+            this.txtboxName.Location = new System.Drawing.Point(136, 73);
+            this.txtboxName.Name = "txtboxName";
+            this.txtboxName.Size = new System.Drawing.Size(263, 22);
+            this.txtboxName.TabIndex = 3;
+            // 
+            // txtboxSurname
+            // 
+            this.txtboxSurname.Location = new System.Drawing.Point(136, 118);
+            this.txtboxSurname.Name = "txtboxSurname";
+            this.txtboxSurname.Size = new System.Drawing.Size(263, 22);
+            this.txtboxSurname.TabIndex = 4;
+            // 
+            // txtboxPhone
+            // 
+            this.txtboxPhone.Location = new System.Drawing.Point(136, 163);
+            this.txtboxPhone.Name = "txtboxPhone";
+            this.txtboxPhone.Size = new System.Drawing.Size(263, 22);
+            this.txtboxPhone.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Surname:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Add Contact";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 25);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Update Info";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(330, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 25);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Delete Contact";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Phone Number:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(519, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(281, 161);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // myConctactsDBDataSet
+            // 
+            this.myConctactsDBDataSet.DataSetName = "MyConctactsDBDataSet";
+            this.myConctactsDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // myConctactsDBDataSetBindingSource
+            // 
+            this.myConctactsDBDataSetBindingSource.DataSource = this.myConctactsDBDataSet;
+            this.myConctactsDBDataSetBindingSource.Position = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 483);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtboxPhone);
+            this.Controls.Add(this.txtboxSurname);
+            this.Controls.Add(this.txtboxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myConctactsDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myConctactsDBDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtboxName;
+        private System.Windows.Forms.TextBox txtboxSurname;
+        private System.Windows.Forms.TextBox txtboxPhone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource myConctactsDBDataSetBindingSource;
+        private MyConctactsDBDataSet myConctactsDBDataSet;
+    }
+}
+
---
+++ ConctactsApp/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ConctactsApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connnection = new SqlConnection("Data Source=DESKTOP-G2ISOTR;Initial Catalog=MyConctactsDB;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void ListOfContacts()
+        {
+            if(connnection.State == ConnectionState.Closed)
+            {
+                connnection.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connnection;
+                cmd.CommandText = "select * from ContactsDB";
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "ContactsDB");
+
+                dataGridView1.DataSource = ds.Tables["ContactsDB"];
+                dataGridView1.Columns[0].Visible = false;
+                connnection.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(connnection.State == ConnectionState.Closed)
+            {
+                connnection.Open();
+                SqlCommand command = new SqlCommand();
+                command.Connection = connnection;
+                command.CommandText = "insert into ContactsDB(contactName, contactSurname, contactPhone) values('" + txtboxName.Text + "', '" + txtboxSurname.Text + "', '" + txtboxPhone.Text + "')";
+                command.ExecuteNonQuery();
+                command.Dispose();
+                connnection.Close();
+                ListOfContacts();
+
+                MessageBox.Show("Contact has been added");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListOfContacts();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure you want to delete", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (connnection.State == ConnectionState.Closed)
+                {
+                    connnection.Open();
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = connnection;
+                    cmd.CommandText = "delete from ContactsDB where contactID=@contactID";
+                    cmd.Parameters.AddWithValue("@contactID", dataGridView1.CurrentRow.Cells[0].Value.ToString());
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+
+                    connnection.Close();
+                    ListOfContacts();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(connnection.State == ConnectionState.Closed)
+            {
+                connnection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connnection;
+                cmd.CommandText = "update ContactsDB set contactName='"+txtboxName.Text+"', contactSurname='"+txtboxSurname.Text+"', contactPhone='"+txtboxPhone.Text+"' where contactID=@contactID";
+                cmd.Parameters.AddWithValue("@contactID", dataGridView1.CurrentRow.Cells[0].Value.ToString());
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+
+                connnection.Close();
+                ListOfContacts();
+                MessageBox.Show("Updated successfully!");
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtboxName.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtboxSurname.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtboxPhone.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+    }
+}
